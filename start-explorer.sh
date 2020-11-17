9292ebd0bc4938edc449e4e1c0877dd5e6e2a9ec
Paul Huang <zedozon@gmail.com>
2020-11-17 23:25:36 +0800

    fix:explorer config

--- start-explorer.sh
+++ start-explorer.sh
@@ -1,7 +1,8 @@
 export EXPLORER=blockchain-explorer
 
 git clone https://github.com/hyperledger/$EXPLORER
-cp first-network.json $PWD/$EXPLORER/examples/net1/connection-profile
+cp explorer-config/first-network.json $PWD/$EXPLORER/examples/net1/connection-profile
+cp explorer-config/docker-compose.yaml $PWD/$EXPLORER
 cp -r $PWD/crypto-config/peerOrganizations $PWD/$EXPLORER/examples/net1/crypto/
 
 cd $EXPLORER
