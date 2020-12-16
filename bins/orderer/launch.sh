ad2cda69a7c6ac1a028e00b2f0a797f343ca61f6
Pocket Bank <pocketbank@Pockets-MacBook-Pro.local>
2020-12-16 20:19:35 +0800

    add orderer tls secret

--- bins/orderer/launch.sh
+++ bins/orderer/launch.sh
@@ -6,4 +6,6 @@ export FABRIC_CFG_PATH=${PWD}/bins/orderer
 export FABRIC_LOGGING_SPEC=WARNING
 export ORDERER_GENERAL_BOOTSTRAPFILE=${PWD}/artefacts/default-genesis.block
 export ORDERER_GENERAL_LOCALMSPDIR=${PWD}/crypto-config/ordererOrganizations/default.com/users/Admin@default.com/msp
+export ORDERER_GENERAL_TLS_PRIVATEKEY=${PWD}/crypto-config/ordererOrganizations/default.com/orderer.default.com/tls/server.key
+export ORDERER_GENERAL_TLS_CERTIFICATE=${PWD}/crypto-config/ordererOrganizations/default.com/orderer.default.com/tls/server.crt
 orderer
