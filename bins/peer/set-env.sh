49e87c86ae475223cce121c5657e545101b087d8
Pocket Bank <pocketbank@Pockets-MacBook-Pro.local>
2020-12-16 21:06:40 +0800

    update peer address

--- bins/peer/set-env.sh
+++ bins/peer/set-env.sh
@@ -7,11 +7,8 @@ export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_TLS_CERT_FILE=${PWD}/crypto-config/peerOrganizations/default.com/peers/devpeer/tls/server.crt
 export CORE_PEER_TLS_KEY_FILE=${PWD}/crypto-config/peerOrganizations/default.com/peers/devpeer/tls/server.key
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/crypto-config/peerOrganizations/default.com/peers/devpeer/tls/ca.crt
-
-
 export CORE_PEER_LOCALMSPID=DefaultMSP
-
 # Admin identity used for commands
 export CORE_PEER_MSPCONFIGPATH=${PWD}/crypto-config/peerOrganizations/default.com/users/Admin@default.com/msp
-
+export CORE_PEER_ADDRESS=54.219.75.228:7051
 export FABRIC_CFG_PATH=${PWD}/bins/peer
