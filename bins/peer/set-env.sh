072f1b16709956b95a3809d6e319ba4579c7a60e
Pocket Bank <pocketbank@Pockets-MacBook-Pro.local>
2020-12-16 21:03:45 +0800

    update peer rootcert tls secret

--- bins/peer/set-env.sh
+++ bins/peer/set-env.sh
@@ -6,7 +6,7 @@ export FABRIC_LOGGING_SPEC=info
 export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_TLS_CERT_FILE=${PWD}/crypto-config/peerOrganizations/default.com/peers/devpeer/tls/server.crt
 export CORE_PEER_TLS_KEY_FILE=${PWD}/crypto-config/peerOrganizations/default.com/peers/devpeer/tls/server.key
-export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/crypto-config/ordererOrganizations/default.com/orderers/orderer.default.com/msp/tlscacerts/tlsca.default.com-cert.pem
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/crypto-config/peerOrganizations/default.com/peers/devpeer/tls/ca.crt
 
 
 export CORE_PEER_LOCALMSPID=DefaultMSP
